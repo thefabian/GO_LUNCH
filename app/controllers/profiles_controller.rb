7a0467d3e664891ad522d8d4fe64b65883d9675b
Matti Fischer <mr.matti.fischer@googlemail.com>
2018-08-21 18:00:56 +0200

    first draft of new profile page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,25 @@
 class ProfilesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :new, :create ]
+  def new
+    @profile = Profile.new
+  end
+
+  def create
+    @profile = Profile.new(profile_params)
+    @profile.user = current_user
+    if @profile.save
+      redirect_to profile_path(@profile)
+    else
+      redirect_to home_path
+    end
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:first_name, :last_name, :job_title, :department, :location, :description, :photo)
+  end
+
 end
+
+
