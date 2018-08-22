981949d7ada88acadbf69abb30273d930a1b815a
Matti Fischer <mr.matti.fischer@googlemail.com>
2018-08-22 15:27:15 +0200

    first draft of creating company form

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "pricing", to: "pages#pricing"
   resources :profiles, only: [ :new, :create, :show ]
+  resources :companies, only: [ :new, :create, :show ]
 end
