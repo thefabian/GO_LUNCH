4bc481b0c83944cf9f01b54a7b9d737bf4b73121
Fabian Bitta <fabian.bitta@gmail.com>
2018-08-23 18:46:55 +0200

    Setup of lunch logic for matching two requests

--- app/controllers/lunches_controller.rb
+++ app/controllers/lunches_controller.rb
@@ -1,2 +1,21 @@
 class LunchesController < ApplicationController
+  def new
+    @lunch = Lunch.new
+  end
+
+  def create
+    @lunch = Lunch.new(request_params)
+    @lunch.request.user = current_user
+    if @lunch.save
+      redirect_to lunch_path(@lunch)
+    else
+      render "lunches/show"
+    end
+  end
+
+  private
+
+  def lunch_params
+    params.require(:lunch).permit(:date, :location, :kind)
+  end
 end
