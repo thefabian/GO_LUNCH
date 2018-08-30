da0e6c6a5d0e3e737e911c10259a0597a1efc022
Fabian Bitta <fabian.bitta@gmail.com>
2018-08-30 15:19:23 +0200

    New seed and style change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,9 +14,9 @@ class ApplicationController < ActionController::Base
     redirect_to(root_path)
   end
 
-  def after_sign_in_path_for(resource)
-    profile_path(current_user)
-  end
+  # def after_sign_in_path_for(resource)
+  #   profile_path(current_user)
+  # end
 
   private
 
